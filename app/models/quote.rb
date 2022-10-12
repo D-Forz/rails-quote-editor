09da3e2920954ba574046ed634626a0fd6a2f49e
duvan <duvanfoz@hotmail.com>
2022-10-12 14:18:15 -0500

    feat: models associations, update fixtures

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ApplicationRecord
-  validates :name, presence: true
+  belongs_to :company
 
+  validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
 
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
